2fbdc773c024eb37741e5da222263b1e89ad6ccc
SGRAVANI <shakuntla.ravani@gmail.com>
2022-12-26 23:37:03 +0530

    portfolio second commit

--- src/Components/Portfolio/Portfolio1.js
+++ src/Components/Portfolio/Portfolio1.js
@@ -6,11 +6,23 @@ import capstone from "../../Assets/capstone.jpg";
 import smartStote from "../../Assets/smartStore-1.jpg";
 import animalSound from "../../Assets/animalsound.jpg";
 import google from "../../Assets/google.jpg";
+import clock from "../../Assets/25+5Clock.png";
+import randomQuote from "../../Assets/random-quote.jpg";
+import calci from "../../Assets/calci.png";
+import markedEditor from "../../Assets/marked-editor.jpg"
 let pData=[{img:findRoute,heading:"Find Route Puzzle using JS",githubRepLink:"https://github.com/SGRAVANI/find-route-puzzle",githubLiveLink:"https://sgravani.github.io/find-route-puzzle/"},
 {img:smartStote,heading:"Ecommerce Web App. with React",githubRepLink:"https://github.com/SGRAVANI/smart-store",githubLiveLink:"https://sgravani.github.io/smart-store/"},
 {img:animalSound,heading:"Animal Sounds for kids",githubRepLink:"https://github.com/SGRAVANI/AnimalSounds4kids/",githubLiveLink:"https://sgravani.github.io/AnimalSounds4kids/" },
 {img:capstone,heading:"HTML+CSS Responsive Template",githubRepLink:"https://github.com/SGRAVANI/capstone-project",githubLiveLink:"https://sgravani.github.io/capstone-project/"},
-{img:google,heading:"Google Landing Page",githubRepLink:"https://github.com/SGRAVANI/mockInterview-LandingPage",githubLiveLink:"https://sgravani.github.io/mockInterview-LandingPage/" }]
+{img:google,heading:"Google Landing Page",githubRepLink:"https://github.com/SGRAVANI/mockInterview-LandingPage",githubLiveLink:"https://sgravani.github.io/mockInterview-LandingPage/" },
+{img:clock,heading:"25+5 Clock using React",githubRepLink:"https://github.com/SGRAVANI/Clock-25-5",githubLiveLink:"https://sgravani.github.io/Clock-25-5/"},
+{img:randomQuote,heading:"Random Quote Generator",githubRepLink:"https://github.com/SGRAVANI/random-quote-generator",githubLiveLink:"https://sgravani.github.io/random-quote-generator/"},
+
+{img:calci,heading:"Calculator using React",githubRepLink:"https://github.com/SGRAVANI/calculator",githubLiveLink:"https://sgravani.github.io/calculator/"},
+
+{img:markedEditor,heading:"Marked Editor using React",githubRepLink:"https://github.com/SGRAVANI/marked-editor",githubLiveLink:"https://sgravani.github.io/marked-editor/"},
+
+]
 let list=pData.map((ele,index)=><PortfolioCard key={index} img={ele.img} heading={ele.heading} githubRepLink={ele.githubRepLink} githubLiveLink={ele.githubLiveLink} />)  
 export default function Portfolio1() {
   return (
